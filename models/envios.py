246d673a5b71b71c2ed633b0f4d19fe3185ca062
xgjimmar945-afk <xgjimmar945@ieshnosmachado.org>
2026-01-21 13:08:26 +0100

    Parte finalizada (falta descomentar y comparar con vehiculo y conductor)

--- models/envios.py
+++ models/envios.py
@@ -5,17 +5,77 @@ class Envios(models.Model):
     _name = "logitrans.envios"
     _description = "Envios"
 
-    nombre = fields.Char(
-        string="Nombre", required=True, help="Nombre"
-    )
+    _rec_name = "nombre"
+
+    nombre = fields.Char(string="Nombre", required=True, help="Nombre")
     fecha_creacion = fields.Date(
         string="Fecha de creación", required=True, help="Fecha de creación"
     )
 
+    fecha_actual = fields.Datetime(
+        string="Fecha Actual",
+        default=fields.Datetime.now,
+    )
+
+    state = fields.Selection(
+        [
+            ("entregado", "Entregado"),
+            ("camino", "En Camino"),
+            ("fabrica", "En Fabrica"),
+        ],
+        "Estado",
+        default="fabrica",
+    )
+
     peso_kg = fields.Integer("Peso en kg")
+
+    total_distancia = fields.Float(
+        string="Distancia Total", compute="_compute_total_distancia"
+    )
+
+    @api.depends("rutas_ids")
+    def _compute_total_distancia(self):
+        for record in self:
+            record.total_distancia = sum(
+                ruta.distancia_estimada for ruta in record.rutas_ids
+            )
+
+    rutas_ids = fields.Many2many(
+        "logitrans.rutas", string="Rutas", required=True, help="Rutas"
+    )
+
+    _sql_constraints = [
+        ("envios_nombre_unique", "UNIQUE (nombre)", "El nombre debe ser único")
+    ]
+
     """ tipos_cargas_id = fields.Many2many(
         "logi_trans.tipos_cargas", string="Tipos de carga"
     )
 
     vehiculo_id = fields.Many2one("logi_trans.vehiculos", string="Vehiculo")
     conductor_id = fields.Many2one("logi_trans.conductores", string="Conductor") """
+
+    @api.constrains("peso_kg")
+    def _check_peso_kg(self):
+        for record in self:
+            if record.peso_kg < 0 or record.peso_kg > 10000:
+                raise models.ValidationError(
+                    "El peso debe ser mayor o igual a 0 y menor o igual a 10000."
+                )
+
+    def btn_borrarRutas(self):
+        self.write({"rutas_ids": [(5, 0, 0)]})
+
+    def btn_submit_to_fabrica(self):
+        self.write({"state": "fabrica"})
+
+    def btn_submit_to_camino(self):
+        self.write({"state": "camino"})
+
+    def btn_submit_to_entregado(self):
+        self.write({"state": "entregado"})
+
+    def btn_generar_informe(self):
+        return self.env.ref("logitrans.logitrans_report_envios_action").report_action(
+            self
+        )
--- models/rutas.py
+++ models/rutas.py
@@ -5,10 +5,14 @@ class Rutas(models.Model):
     _name = "logitrans.rutas"
     _description = "Rutas"
 
+    _rec_name = "tipo_ruta"
+
     tipo_ruta = fields.Selection(
         [
-            ("ruta", "Ruta"),
-            ("ruta", "Ruta"),
+            ("Urbana", "Urbana / Local"),
+            ("Provincial", "Provincial"),
+            ("Nacional", "Nacional"),
+            ("Internacional", "Internacional"),
         ],
         string="Tipo de ruta",
         required=True,
@@ -17,4 +21,41 @@ class Rutas(models.Model):
     distancia_estimada = fields.Float(
         string="Distancia estimada", required=True, help="Distancia estimada"
     )
-    
+
+    destino_ubicacion_id = fields.Many2one(
+        "logitrans.ubicacion", string="Destino", help="Destino"
+    )
+    origen_ubicacion_id = fields.Many2one(
+        "logitrans.ubicacion", string="Origen", help="Origen"
+    )
+
+    envios_ids = fields.Many2many(
+        "logitrans.envios", string="Envios", required=True, help="Envios"
+    )
+
+    @api.onchange("origen_ubicacion_id", "destino_ubicacion_id")
+    def _onchange_ubicaciones(self):
+        if self.origen_ubicacion_id and self.destino_ubicacion_id:
+            if self.origen_ubicacion_id == self.destino_ubicacion_id:
+                return {
+                    "warning": {
+                        "title": "Error",
+                        "message": "Origen y Destino no pueden ser iguales.",
+                    }
+                }
+
+            if (
+                self.origen_ubicacion_id.provincia
+                == self.destino_ubicacion_id.provincia
+            ):
+                self.tipo_ruta = "Provincial"
+            else:
+                self.tipo_ruta = "Nacional"
+
+    def btn_borrarUbicacionOrigen(self):
+        for record in self:
+            record.origen_ubicacion_id = False
+
+    def btn_borrarUbicacionDestino(self):
+        for record in self:
+            record.destino_ubicacion_id = False
